5a55b046b7fc75f2cb71f715517a721492c35707
Revanth Reddy <revanthkumar95@gmail.com>
2016-05-14 21:57:06 +0530

    In this example, we will discuss on the analysis of travel dataset and gain insights from the dataset using Apache Spark.
    The travel dataset is publically available and the contents are detailed
    under the heading, ‘Travel Sector Dataset Description’.
    
    Based on the data, we will find the top 20 destination people travel the
    most, top 20 locations from where people travel the most, top 20 cities
    that generate high airline revenues for travel, based on booked trip
    count.
    
    Travel Sector Dataset Description
    
    Column 1: City pair (Combination of from and to): String
    
    Column 2: From location: String
    
    Column 3: To Location: String
    
    Column 4: Product type: Integer (1=Air, 2=Car, 3 =Air+Car, 4 =Hotel,
    5=Air+Hotel, 6=Hotel +Car, 7 =Air+Hotel+Car)
    
    Column 5: Adults traveling: Integer
    
    Column 6: Seniors traveling: Integer
    
    Column 7: Children traveling: Integer
    
    Column 8: Youth traveling: Integer
    
    Column 9: Infant traveling: Integer
    
    Column 10: Date of travel: String
    
    Column 11: Time of travel: String
    
    Column 12: Date of Return: String
    
    Column 13: Time of Return: String
    
    Column 14: Price of booking: Float
    
    Column 15: Hotel name: String

---
+++ spark-scala/src/main/scala/com/spark/usecases/TravelDataAnalysis.scala
@@ -0,0 +1,33 @@
+package com.spark.usecases
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions._
+object TravelDataAnalysis {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Travel-Data-Analysis").setMaster("local[1]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val textFile = sc.textFile("E:/Software/Spark/data/TravelData.txt")
+    val travelDataRDD = textFile.mapPartitions(_.drop(1)) //remove the header information from the file
+    
+    //Top 20 destination people travel the most  
+    val top20DestinationRDD = travelDataRDD.map(lines => lines.split('\t')).
+      map(x => (x(2), 1)).reduceByKey(_ + _).
+      map(item => item.swap).sortByKey(false).take(20)
+    top20DestinationRDD.foreach(f => println(f))
+
+    //Top 20 locations from where people travel the most
+    val top20LocationsRDD = travelDataRDD.map(lines => lines.split('\t')).map(x => (x(1), 1)).
+      reduceByKey(_ + _).map(item => item.swap).sortByKey(false).take(20)
+
+    top20LocationsRDD.foreach(f => println(f))
+
+    //Top 20 cities that generate high airline revenues for travel
+    val fil = travelDataRDD.map(x => x.split('\t')).filter(x => { if ((x(3).matches(("1")))) true else false })
+    // fil.collect().foreach { x => println(x(2)) }
+    val Top20Cities = fil.map(x => (x(2), 1)).reduceByKey(_ + _).map(item => item.swap).sortByKey(false).take(20)
+    Top20Cities.foreach(f => println(f))
+  }
+
+  //https://acadgild.com/blog/spark-use-case-travel-data-analysis/
+}
