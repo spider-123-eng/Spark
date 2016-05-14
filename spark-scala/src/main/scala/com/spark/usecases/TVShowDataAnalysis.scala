4ff3c9a7d0c920a9a4f66c3bead5ab82b6245097
Revanth Reddy <revanthkumar95@gmail.com>
2016-05-14 22:43:07 +0530

    In this example we will be  taking a famous Tv show dataset i.e., The Daily show and we will be performing analysis on the guests who came t
    Dataset Description:
    
    YEAR –  The year the episode aired
    
    GoogleKnowlege_Occupation -Their occupation or office, according to
    Google’s Knowledge Graph or, if they’re not in there, how Stewart
    introduced them on the program.
    
    Show – Air date of episode. Not unique, as some shows had more than one
    guest
    
    Group – A larger group designation for the occupation. For instance, us
    senators, us presidents, and former presidents are all under
    “politicians”
    
    Raw_Guest_List – The person or list of people who appeared on the show,
    according to Wikipedia. The GoogleKnowlege_Occupation only refers to one
    of them in a given row.

---
+++ spark-scala/src/main/scala/com/spark/usecases/TVShowDataAnalysis.scala
@@ -0,0 +1,23 @@
+package com.spark.usecases
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object TVShowDataAnalysis {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("TV-Show-Data-Analysis").setMaster("local[1]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val format = new java.text.SimpleDateFormat("MM/dd/yy")
+    val textFile = sc.textFile("E:/Software/Spark/data/dialy_show_guests.txt")
+    val tvDataRDD = textFile.mapPartitions(_.drop(1)) //remove the header information from the file
+
+    //Find the top 5 kinds of GoogleKnowlege_Occupation people guested the show in a particular time period.
+    
+    val splitedRDD = tvDataRDD.map(line => line.split(","))
+    val pair = splitedRDD.map(line => (line(1), format.parse(line(2))))
+    val fil = pair.filter(x => { if (x._2.after(format.parse("1/11/99")) && x._2.before(format.parse("6/11/99"))) true else false })
+    val top5GuestsRDD = fil.map(x => (x._1, 1)).reduceByKey(_ + _).map(item => item.swap).sortByKey(false).take(5)
+
+    top5GuestsRDD.foreach(f => println(f))
+  }
+}
