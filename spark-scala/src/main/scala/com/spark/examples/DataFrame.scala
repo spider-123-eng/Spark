3cf34611218ef7e21c26b8f58d8bbd14eb020f71
Revanth Reddy <revanthkumar95@gmail.com>
2016-05-25 20:20:07 +0530

    Adding extra column to DataFrame

--- spark-scala/src/main/scala/com/spark/examples/DataFrame.scala
+++ spark-scala/src/main/scala/com/spark/examples/DataFrame.scala
@@ -5,13 +5,14 @@ import org.apache.spark.sql.functions._
 import com.spark.util._
 object DataFrame {
   case class Employee(empid: Int, name: String, dept: String, salary: Int, nop: Int)
+  case class AggregatedEmpData(empid: Int, name: String, dept: String, sumsalary: Long, sumnop: Long, maxsalary: Int, date: String)
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Spark-DataFrame").setMaster("local[1]")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
 
-    val empDataRDD = sc.textFile(args(0)) //path to emp.txt
+    val empDataRDD = sc.textFile("E:/Software/Spark/data/emp.txt") //path to emp.txt
     val dropHeaderRDD = empDataRDD.mapPartitions(_.drop(1)) //remove the header information from the file
 
     /*println(dropHeaderRDD.first())
@@ -29,6 +30,11 @@ object DataFrame {
     //Spark Aggregations 
     val aggDF = empDF.groupBy("empid", "name", "dept").
       agg(sum(empDF.col("salary")), sum(empDF.col("nop")), max(empDF.col("salary")))
+    aggDF.printSchema()
+
+    //Adding extra column at the end ..
+    val finalDF = aggDF.map(row => AggregatedEmpData(row.getInt(0), row.getString(1), row.getString(2), row.getLong(3), row.getLong(4), row.getInt(5), Utills.getTime()))
+    println(finalDF.first())
 
     //Saving data as text file
     aggDF.rdd.coalesce(1, false).saveAsTextFile("F:/Software/Spark/data/aggData/" + Utills.getTime())
